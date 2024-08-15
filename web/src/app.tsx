e7271f7feb6f8a6fea20f43b85c47ef1d4d0ab5f
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-08-15 17:51:08 -0300

    [WIP]

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,24 +1,26 @@
+import { Toaster } from 'sonner'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
-import { Toaster } from 'sonner'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 const router = createBrowserRouter([
-   {
-      path:'/',
-      element: <CreateRoom/>
-   },
-   {
-      path:'/room/:roomId',
-      element: <Room/>
-   }
+  {
+    path: '/',
+    element: <CreateRoom />
+  },
+  {
+    path: '/room/:roomId',
+    element: <Room />
+  }
 ])
 
 export function App() {
-   return  (
-      <>
-         <RouterProvider router={router} />
-         <Toaster invert richColors/>
-      </>
-   )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </QueryClientProvider>
+  )
 }
